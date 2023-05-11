36d51cc68b4d0328f71ccae04f9912d31a055837
hassan <hzangene5@gmail.com>
2023-05-11 19:46:45 +0430

    creat project

---
+++ app/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+// Load Config
+require_once 'config/config.php';
+
+
+
+require_once 'helpers/url_helper.php';
+require_once 'helpers/session_helper.php';
+
+function article_mvc($calassName){
+    require_once 'libraries/'. $calassName .'.php';
+}
+
+spl_autoload_register('article_mvc');
---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'article_mvc');
+
+
+// App Root
+define('APPROOT', dirname(__DIR__));
+// URL ROOT
+define('URLROOT', 'http://localhost/articlemvc');
+// Site Name
+define('SITENAME','articlemvc.ir');
