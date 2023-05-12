2906a5b60bcf368763cdf8525cfa86f1ae784e3a
hassan <hzangene5@gmail.com>
2023-05-13 03:18:46 +0430

    create model article

--- app/controllers/Articles.php
+++ app/controllers/Articles.php
@@ -1,18 +1,29 @@
 <?php
 class Articles extends Controller
 {
+    protected $articleModel;
 
     public function __construct()
     {
     
+        if (!isLoggedIn()) {
+            redirect('users/login');
+        }
+        $this->articleModel = $this->model('Article');
+
+
     }
 
     public function index()
     {
-
-        $data = [ ];
+        // Get Article
+        $articles = $this->articleModel->getArticle();
+        $data = [
+            'articles' => $articles
+        ];
 
         $this->view('articles/index', $data);
     }
+
 
 }
